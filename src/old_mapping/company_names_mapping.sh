de9f84165e3a54ca70ce5ba528eba1915c92764e
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-05-09 04:59:29 +0000

    Cleanup old mapping technology
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/old_mapping/company_names_mapping.sh
+++ src/old_mapping/company_names_mapping.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This uses merged-company-names-mapping
 ./sort_configs.sh
 ruby ./company_names_mapping.rb merged-company-names-mapping cncf-config/email-map all_affs.csv github_users.json
 #ruby ./company_names_mapping.rb company-names-mapping cncf-config/email-map all_affs.csv github_users.json
