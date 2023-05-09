bca45624bc0ed728716727f10936cff63da01a92
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-05-09 05:25:33 +0000

    Post process-update
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/sort_configs.sh
+++ src/sort_configs.sh
@@ -6,7 +6,8 @@ cat email-map | sort | uniq > out; mv out email-map
 cat domain-map | sort | uniq > out; mv out domain-map
 cat aliases | sort | uniq > out; mv out aliases
 cd ..
-cat company-names-mapping | sort | uniq > out; mv out company-names-mapping
+# No more needed with 2nd mapping approach
+# cat company-names-mapping | sort | uniq > out; mv out company-names-mapping
 sed -i '1d' all_affs.csv
 cat all_affs.csv | sort | uniq > out
 echo '"email","name","company","date_to","source"' > all_affs.csv
