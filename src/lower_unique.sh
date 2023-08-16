db0b96bcd72c6158cdf4ebe3abe471528aab1d9b
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-08-16 07:20:16 +0000

    Updates to JSON sort
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- src/lower_unique.sh
+++ src/lower_unique.sh
@@ -6,3 +6,4 @@ then
 fi
 grep -HIn '\w@\w' "${1}"
 cp "${1}" out && vim -c "%s/[A-Z]/\L&/g|%s/\([a-zA-Z0-9]\)@\([a-zA-Z0-9]\)/\1!\2/g|w|q" out && cat out | sort > out1 && cat out | sort | uniq > out2 && diff out1 out2 && rm -f out out1 out2
+grep -E '[A-Za-z0-9._%+-]+\![A-Za-z0-9.-]+\.[A-Za-z]{2,6}\s*,\s*[A-Za-z0-9._%+-]+\![A-Za-z0-9.-]+\.[A-Za-z]{2,6}\s+[A-Za-z0-9._%+-]+' "${1}"
--- src/sort_json.rb
+++ src/sort_json.rb
@@ -1,10 +1,31 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'date'
+# require 'pry'
 
 def sort_json(json_file)
   data = JSON.parse File.read json_file
   data = data.sort_by { |u| [-u['commits'], u['login'], u['email']] }
+  data.each_with_index do |row, idx|
+    affs = row['affiliation']
+    next unless affs
+    ary = affs.split(/\s*,\s*/)
+    next if ary.length <= 1
+    ary = ary.sort_by do |aff|
+      ary2 = aff.split(/\s*<\s*/)
+      dts = ary2.length >= 2 ? ary2[1] : '2100-01-01'
+      Date.strptime(dts,"%Y-%m-%d").to_time.to_i
+    end
+    affs2 = ary.join(', ')
+    if affs != affs2
+      p [idx, row['login'], row['email']]
+      puts affs
+      puts affs2
+      puts ''
+      data[idx]['affiliation'] = affs
+    end
+  end
   pretty = JSON.pretty_generate data
   File.write json_file, pretty
 end
